1c8cde2fc85b1e10d5b401826ddbfab204e90d9b
Deimantas Gedgaudas <gedgaudasdei@gmail.com>
2019-10-25 21:44:26 +0200

    Move ChronometerScreen to separate file

--- App.js
+++ App.js
@@ -4,116 +4,8 @@ import { createAppContainer } from 'react-navigation'
 import { createBottomTabNavigator } from 'react-navigation-tabs'
 import TimerScreen from './Screens/TimerScreen'
 import ClockScreen from './Screens/ClockScreen'
-import Timer from './Components/ChronometerComponents/Timer'
-import RoundButton from './Components/ChronometerComponents/RoundButton'
-import ButtonsRow from './Components/ChronometerComponents/ButtonsRow'
-import LapsTable from './Components/ChronometerComponents/LapsTable'
+import ChronometerScreen from './Screens/ChronometerScreen'
 
-
-export class ChronometerScreen extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      laps: [],
-      start: 0,
-      now: 0,
-    }
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timer)
-  }
-
-  start = () => {
-    const now = new Date().getTime()
-
-    this.setState({
-      start: now,
-      now,
-      laps: [0],
-    })
-
-    this.timer = setInterval(() => {
-      this.setState({ now: new Date().getTime()})
-    }, 100)
-  }
-
-  stop = () => {
-    clearInterval(this.timer)
-    const { laps, now, start } = this.state
-    const [firstLap, ...other] = laps
-
-    this.setState({
-      laps: [firstLap + now - start, ...other],
-      start: 0,
-      now: 0,
-    })
-  }
-
-  lap = () => {
-    const timeStamp = new Date().getTime()
-    const { laps, now, start } = this.state
-    const [firstLap, ...other] = laps
-
-    this.setState({
-      laps: [0, firstLap + now - start, ...other],
-      start: timeStamp,
-      now: timeStamp,
-    })
-  }
-
-  reset = () => {
-    this.setState({
-      laps: [],
-      start: 0,
-      now: 0,
-    })
-  }
-
-  resume = () => {
-    const now = new Date().getTime()
-
-    this.setState({
-      start: now,
-      now
-    })
-
-    this.timer = setInterval(() => {
-      this.setState({ now: new Date().getTime()})
-    }, 100)
-  }
-
-  render() {
-    const { laps, start, now } = this.state
-    const timer = now - start
-    return (
-      <View style={styles.container}>
-        <Timer 
-          interval={laps.reduce((total, curr) => total + curr, 0) + timer} 
-          style={styles.timer}/>
-        {laps.length === 0 && (
-          <ButtonsRow>
-            <RoundButton title='Lap' color='#8B8B90' background='#151515' disabled></RoundButton>
-            <RoundButton title='Start' color='#50D167' background='#1B361F' onPress={this.start}></RoundButton>
-          </ButtonsRow>
-        )}
-        {start > 0 && (
-          <ButtonsRow>
-            <RoundButton title='Lap' color='#FFFFFF' background='#3D3D3D' onPress={this.lap}></RoundButton>
-            <RoundButton title='Stop' color='#E33935' background='#3C1715' onPress={this.stop}></RoundButton>
-          </ButtonsRow>
-        )}
-        {laps.length > 0 && start === 0 && (
-          <ButtonsRow>
-            <RoundButton title='Reset' color='#FFFFFF' background='#3D3D3D' onPress={this.reset}></RoundButton>
-            <RoundButton title='Resume' color='#50D167' background='#1B361F' onPress={this.resume}></RoundButton>
-          </ButtonsRow>
-        )}
-        <LapsTable laps={laps} timer={timer}/>
-      </View>
-    )
-  }
-}
 
 const TabNavigator = createBottomTabNavigator({
   Timer: TimerScreen,
@@ -124,17 +16,5 @@ const TabNavigator = createBottomTabNavigator({
 export default createAppContainer(TabNavigator)
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#0D0D0D',
-    alignItems: 'center',
-    paddingTop: 150,
-    paddingHorizontal: 20,
-  },
-  timer: {
-    color: "#FFFFFF",
-    fontSize: 46,
-    fontWeight: '100',
-    width: 75,
-  }
-});
+
+})
