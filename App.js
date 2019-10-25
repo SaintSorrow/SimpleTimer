ddd3c71e49ab9d46b5f3f43047fbc30cd497e0dc
Deimantas Gedgaudas <gedgaudasdei@gmail.com>
2019-10-25 23:12:19 +0200

    Use material UI component for bottom tab navigator

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { createAppContainer } from 'react-navigation'
-import { createBottomTabNavigator } from 'react-navigation-tabs'
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs'
 import TimerScreen from './Screens/TimerScreen'
 import ClockScreen from './Screens/ClockScreen'
 import ChronometerScreen from './Screens/ChronometerScreen'
 
-
-const TabNavigator = createBottomTabNavigator({
-  Timer: TimerScreen,
-  Chronometer: ChronometerScreen,
-  Clock: ClockScreen,
-});
+const TabNavigator = createMaterialBottomTabNavigator(
+  {
+    Timer: { screen: TimerScreen },
+    Chronometer: { screen: ChronometerScreen },
+    Clock: { screen: ClockScreen }
+  },
+  {
+    initialRouteName: 'Timer',
+    activeColor: '#F0EDF6',
+    inactiveColor: '#3E2465',
+    barStyle: { backgroundColor: '#694FAD'},
+  }
+)
 
 export default createAppContainer(TabNavigator)
 
--- Components/ChronometerComponents/LapsTable.js
+++ Components/ChronometerComponents/LapsTable.js
@@ -49,7 +49,7 @@ const styles = StyleSheet.create({
     alignSelf: 'stretch',
   },
   lap: {
-    flexDirection: 'row',
+    flexDirection: 'row-reverse',
     justifyContent: 'space-between',
     borderColor: '#151515',
     borderTopWidth: 1,
